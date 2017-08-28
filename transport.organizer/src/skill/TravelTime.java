71f9c30be96eb0aaf0ae604b57bc7d4cd4d37cdc
ThibautDemare <fdhp_76@hotmail.com>
2017-08-28 13:52:38 +0200

    Some fix to compute edge cost

--- transport.organizer/src/skill/TravelTime.java
+++ transport.organizer/src/skill/TravelTime.java
@@ -10,40 +10,43 @@ public class TravelTime implements NumberProvider {
 	// la longueur * la vitesse du véhicule mais il faut aussi rajouter au temps de trajet
 	// le temps de manutention au sein des nœuds + éventuellement le temps d'attente du départ du véhicule
 	private final IScope scope;
-	
+
 	public TravelTime(final IScope scope){
 		this.scope = scope;
 	}
-	
+
 	public double getEdgeCost(Node source, Edge e, Node dest) {
 		double res = 0;
 		// first, we add the time to cross the edge
 		if(e.hasAttribute("length") && e.hasAttribute("speed")){
-			res += e.getNumber("length") * e.getNumber("speed");
+			res += e.getNumber("length") / e.getNumber("speed");
 		}
 		else {
 			// Default edge cost if length and speed have not been declared
-			res += Math.hypot(source.getNumber("x")-dest.getNumber("x"), source.getNumber("y")-dest.getNumber("y")) * 50;
+			res += Math.hypot(source.getNumber("x")-dest.getNumber("x"), source.getNumber("y")-dest.getNumber("y"))/1000 / 50;
 		}
 
 		// second, get the time to leave the source node and to enter the edge
-		if(source.hasAttribute("handling_time_to_"+e.getAttribute("graph_type"))){
+		if(dest.hasAttribute("handling_time_from_"+e.getAttribute("graph_type"))){
 			// We first get the handling time
-			double handlingTime = source.getNumber("handling_time_to_"+e.getAttribute("graph_type"));
-			
+			double handlingTime = dest.getNumber("handling_time_from_"+e.getAttribute("graph_type"));
 //TODO
 //			// Then, and based on this handling time and the frequencies of vehicle departure, we determine when the goods can leave.
 //			final IAgent agent = getCurrentAgent(scope);
 //			IAgent transporter = (IAgent) agent.getAttribute("transporter_"+e.getAttribute("graph_type"));
 //			double timeBeforeDeparture = 0;
-
 			res += handlingTime;
 		}
-		// third, get the handling time to leave the edge and to enter the dest node
-		if(dest.hasAttribute("handling_time_from_"+e.getAttribute("graph_type"))){
-			res += source.getNumber("handling_time_from_"+e.getAttribute("graph_type"));
+		else if(source.hasAttribute("handling_time_to_"+e.getAttribute("graph_type"))){
+			// We first get the handling time
+			double handlingTime = source.getNumber("handling_time_to_"+e.getAttribute("graph_type"));
+//TODO
+//			// Then, and based on this handling time and the frequencies of vehicle departure, we determine when the goods can leave.
+//			final IAgent agent = getCurrentAgent(scope);
+//			IAgent transporter = (IAgent) agent.getAttribute("transporter_"+e.getAttribute("graph_type"));
+//			double timeBeforeDeparture = 0;
+			res += handlingTime;
 		}
-
 		return res;
 	}
 }
