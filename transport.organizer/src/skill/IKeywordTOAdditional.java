29f9487da127874611542aee7f39708797049885
ThibautDemare <fdhp_76@hotmail.com>
2018-01-10 19:02:36 +0100

    Add some new keywords and remove 'volume'

--- transport.organizer/src/skill/IKeywordTOAdditional.java
+++ transport.organizer/src/skill/IKeywordTOAdditional.java
@@ -4,6 +4,7 @@ import msi.gama.common.interfaces.IKeyword;
 
 public class IKeywordTOAdditional implements IKeyword {
 	public static final String TRANSPORT_ORGANIZER = "TransportOrganizer";
+	public static final String TRANSPORTER = "Transporter";
 	public static final String NETWORK = "network";
 	public static final String MODE = "mode";
 	public static final String NETWORKS = "networks";
@@ -11,5 +12,5 @@ public class IKeywordTOAdditional implements IKeyword {
 	public static final String ORIGIN = "origin";
 	public static final String DESTINATION = "destination";
 	public static final String STRATEGY = "strategy";
-	public static final String VOLUME = "volume";
+	public static final String COMMODITY = "commodity";
 }
