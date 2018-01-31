8a71297540d163b77dd202bc310292c59f028974
ThibautDemare <fdhp_76@hotmail.com>
2018-01-31 08:45:29 +0100

    Add some parameters to the method to be able to compute edge cost

--- transport.organizer/src/skill/NumberProvider.java
+++ transport.organizer/src/skill/NumberProvider.java
@@ -4,6 +4,6 @@ import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
 
 public interface NumberProvider {
-	public double getEdgeCost(Node source, Edge e, Node dest);
+	public double getEdgeCost(Node source, Edge e, Node dest, double currentDistance, double volume, Node previousMultiModalNode);
 }
 
