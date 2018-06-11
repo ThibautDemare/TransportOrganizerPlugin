6f9f69cc955adab2a6ea9d4fb11cc29d07dd099e
ThibautDemare <fdhp_76@hotmail.com>
2018-06-11 11:32:02 +0200

    Implement the getEdgeCost method for FinancialCosts

--- transport.organizer/src/skill/FinancialCosts.java
+++ transport.organizer/src/skill/FinancialCosts.java
@@ -3,15 +3,22 @@ package skill;
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Node;
 
-public class FinancialCosts implements NumberProvider {
+import msi.gama.metamodel.agent.IAgent;
+import msi.gama.runtime.IScope;
+import msi.gama.util.GamaDate;
+
+public class FinancialCosts extends NumberProvider {
+	public FinancialCosts(final TransportOrganizerSkill tos, final IScope scope){
+		super(tos, scope);
+	}
+
 	@Override
-	public double getEdgeCost(Node source, Edge e, Node dest) {
-		// TODO Auto-generated method stub
-		return 0;
+	public double getEdgeCost(Node source, Edge e, Node dest, double volume) {
+		final IAgent transporter = tos.getTransporter(scope, e.getAttribute("graph_type"));
+		return e.getNumber("length") * volume * TransporterSkill.getVolumeKilometerCosts(scope, transporter);
 	}
 
 	@Override
 	public double getMultiModalCost(Node source, Node dest, String mode, double currentDistance, double volume) {
-		// TODO Auto-generated method stub
 		return 0;
 	}}
