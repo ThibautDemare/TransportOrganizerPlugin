9d1a720d0d081acdd8d684d84e9d75e4a8efa9d6
ThibautDemare <fdhp_76@hotmail.com>
2019-01-24 15:42:48 +0100

    Remove unwanted tabs

--- transport.organizer/src/skill/MultiModalDijkstra.java
+++ transport.organizer/src/skill/MultiModalDijkstra.java
@@ -256,7 +256,6 @@ public class MultiModalDijkstra extends AbstractSpanningTree {
 				data.edgeFromParent = null;
 			}
 			node.removeAttribute(resultAttribute);
-
 		}
 
 		for(Edge e : graph.getEachEdge()) {
