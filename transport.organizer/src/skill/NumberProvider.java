8dc85b568cb4e54141b5ce8fe800bc225987a87c
ThibautDemare <fdhp_76@hotmail.com>
2019-02-07 14:20:26 +0100

    Clean code

--- transport.organizer/src/skill/NumberProvider.java
+++ transport.organizer/src/skill/NumberProvider.java
@@ -64,15 +64,13 @@ public abstract class NumberProvider {
 		if(e.hasAttribute("blocked_edge") && (boolean)e.getAttribute("blocked_edge"))
 			return Double.POSITIVE_INFINITY;
 
-		double res = 0;
 		if(e.hasAttribute("length") && e.hasAttribute("speed")){
-			res += e.getNumber("length") / e.getNumber("speed");
+			return e.getNumber("length") / e.getNumber("speed");
 		}
 		else {
 			// Default edge cost if length and speed have not been declared
-			res += Math.hypot(source.getNumber("x")-dest.getNumber("x"), source.getNumber("y")-dest.getNumber("y"))/1000 / 50;
+			return (Math.hypot(source.getNumber("x")-dest.getNumber("x"), source.getNumber("y")-dest.getNumber("y"))/1000.0) / 50.0;
 		}
-		return res;
 	}
 
 	/**
@@ -87,29 +85,25 @@ public abstract class NumberProvider {
 	public double getTimeMultiModalCost(Node source, Node dest, String mode, double currentDistance, double volume) {
 		double res = 0;
 		if(((IAgent)dest.getAttribute("gama_agent")).getAttribute("handling_time_from_"+mode) != null) {
-			// We first get the handling time
-			double handlingTime = (double)((IAgent)dest.getAttribute("gama_agent")).getAttribute("handling_time_from_"+mode);
-			res += handlingTime;
+			// We need to know when the next available vehicle leaves the previous multimodal node (the source node)
 
-			// Then, we need to know when the next available vehicle leaves the previous multimodal node
-			// To do so, we need to get the transporter on the next edge
-			final IAgent transporter;
-			// Here, I get the transporter agent.
-			transporter = tos.getTransporter(scope, mode);
-			handlingTime = (double)((IAgent)source.getAttribute("gama_agent")).getAttribute("handling_time_to_"+mode);
+			// First, we get the date when the goods arrive to the source node
 			GamaDate currentDate = scope.getClock().getCurrentDate().plusMillis(currentDistance*3600*1000);
+			// We get the handling time to make the goods leave the source node and add this duration to the "currentDate"
+			double handlingTime = (double)((IAgent)source.getAttribute("gama_agent")).getAttribute("handling_time_to_"+mode);
 			GamaDate minimalDepartureDate = currentDate.plusMillis(handlingTime*3600*1000);
-			// But this object is not an instance of TransporterSkill, therefore, I need to use a static method with the agent in parameter
+
+			// Then, we can determine the departure date of the vehicle. To do so, we need to get the transporter agent on the next edge
+			final IAgent transporter = tos.getTransporter(scope, mode);
+			// But "transporter" is not an instance of TransporterSkill, therefore, I need to use a static method with the agent in parameter
 			GamaDate departure = TransporterSkill.getDepartureDate(scope, transporter, source, dest, minimalDepartureDate, volume);
-			if(departure.getSecond() != 0){
-				departure.plus(60-departure.getSecond(), ChronoUnit.SECONDS);
-			}
-			if(departure.getMinute() != 0 ){
-				departure.plus(60-departure.getMinute(), ChronoUnit.MINUTES);
-			}
-			// Now, I know the departure date. I compute the time between now and the departure, and convert it in hours
+
+			// Now, I know the departure date. I compute the time between the currentDate and the departure, and convert it in hours
 			double hours = currentDate.until(departure, ChronoUnit.HOURS);
 			res += hours;
+
+			// Eventually, we add the time to enter the destination node
+			res += (double)((IAgent)dest.getAttribute("gama_agent")).getAttribute("handling_time_from_"+mode);
 		}
 		return res;
 	}
