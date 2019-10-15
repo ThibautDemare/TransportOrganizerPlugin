20875014795122e9d855b3a05d02c8f311868a60
ThibautDemare <fdhp_76@hotmail.com>
2019-10-15 17:24:57 +0200

    Take into account the time between vehicles also for financial cost when this time is set to infinity (making the path impossible)

--- transport.organizer/src/skill/FinancialCosts.java
+++ transport.organizer/src/skill/FinancialCosts.java
@@ -13,12 +13,19 @@ public class FinancialCosts extends NumberProvider {
 	}
 
 	@Override
-	public double getEdgeCost(Node source, Edge e, Node dest, double volume) {
+	public Double getEdgeCost(Node source, Edge e, Node dest, double volume) {
 		return getFinancialCostLength(source, e, dest, volume);
 	}
 
 	@Override
-	public double getMultiModalCost(Node source, Node dest, String mode, double currentDistance, double volume) {
-		return 0;
+	public Double getMultiModalCost(Node source, Node dest, String mode, double currentDistance, double volume) {
+		// We first check if the constraint of time between vehicles allows some departure between the source and the destination
+		//To do so, we need to get the transporter agent on the next edge
+		final IAgent transporter = tos.getTransporter(scope, mode);
+		if(TransporterSkill.getTimeBeweenVehicleDest(scope, transporter, source.getAttribute("gama_agent"), dest.getAttribute("gama_agent")) == Double.POSITIVE_INFINITY){
+			return Double.POSITIVE_INFINITY;
+		}
+		// If there is no constraint, then, we return a cost of one here.
+		return 1.0;
 	}
 }
